17b6eeb82345726f0d51abe0d0080f8105765c49
mayuming <maym@appexnetworks.com>
2024-06-23 13:38:37 +0000

    support fast_switch_ant_inventory APIs

--- E710_APIs.py
+++ E710_APIs.py
@@ -1,7 +1,10 @@
+# E710_APIs.py
+
 import time
 import queue
 from pyImpinj import ImpinjR2KReader
 from pyImpinj.constant import READER_ANTENNA
+from pyImpinj.enums    import ImpinjR2KFastSwitchInventory
 
 class RFIDReader:
     def __init__(self, port='/dev/ttyS3', antennas=None):
@@ -33,18 +36,59 @@ class RFIDReader:
             print(err)
             raise  # 重新抛出异常以便 .NET Core 可以捕捉
 
+
+
+    def switch_ant_inventory(self):
+        # 初始化 param 字典
+        param = dict(Interval=0, Repeat=1)
+        antenna_parameters = {
+            'ANTENNA1': ('A', 'Aloop'),
+            'ANTENNA2': ('B', 'Bloop'),
+            'ANTENNA3': ('C', 'Cloop'),
+            'ANTENNA4': ('D', 'Dloop')
+        }
+
+        # 根据 self.antennas 列表中的天线动态构建 param
+        for index, antenna in enumerate(self.antennas, start=1):
+            if antenna in antenna_parameters:
+                key, loop_key = antenna_parameters[antenna]
+                param[key] = getattr(ImpinjR2KFastSwitchInventory, antenna)
+                param[loop_key] = 1  # 或者其他你希望设定的 loop 值
+        return param  # 返回构建好的 param 字典
+
     def read_and_process(self):
+        param = self.switch_ant_inventory()
+        print(param)
         try:
             while self.running:
                 try:
                     data = self.TAG_QUEUE.get(timeout=0.2)
+                #except BaseException:
                 except queue.Empty:
-                    self.R2000.rt_inventory(repeat=1)
+                    self.R2000.fast_switch_ant_inventory(param=param)
                     continue
 
-                time.sleep(1)  # 延迟以确保数据读取稳定
-                if 'DONE' != data.get('type') and self.callback:
-                    self.callback(data)  # 调用回调函数处理数据
+                self.callback(data)  # 调用回调函数处理数据
+        finally:
+            self.cleanup()
+
+
+
+    def read_and_process_switch(self):
+        try:
+            while self.running:
+                for antenna in self.antennas:
+                    self.R2000.set_work_antenna(READER_ANTENNA[antenna])
+
+                    try:
+                        data = self.TAG_QUEUE.get(timeout=0.2)
+                    except queue.Empty:
+                        self.R2000.rt_inventory(repeat=1)
+                        continue
+
+                    time.sleep(1)  # 延迟以确保数据读取稳定
+                    if 'DONE' != data.get('type') and self.callback:
+                        self.callback(data)  # 调用回调函数处理数据
         finally:
             self.cleanup()
 
@@ -67,6 +111,16 @@ if __name__ == "__main__":
 
     # 创建 RFIDReader 实例，指定串口和天线
     reader = RFIDReader(port='/dev/ttyS3', antennas=['ANTENNA1', 'ANTENNA2', 'ANTENNA3', 'ANTENNA4'])
-    #reader = RFIDReader(port='/dev/ttyS3')
+    #reader = RFIDReader(port='/dev/ttyS3', antennas=['ANTENNA1'])
     reader.set_callback(test_callback)
     reader.initialize()
+
+
+    try:
+        reader.read_and_process()  # 开始处理数据
+    except KeyboardInterrupt:  # 允许使用 Ctrl+C 来停止程序
+        pass
+    finally:
+        reader.stop()  # 停止读取数据
+        reader.cleanup()  # 清理资源
+
